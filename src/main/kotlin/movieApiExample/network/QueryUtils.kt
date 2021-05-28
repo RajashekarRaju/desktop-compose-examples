5099c065628f84980fee6e6aa61967ae72fd60f9
rajas <rajashekarraju123@gmail.com>
2021-05-29 00:14:52 +0530

    Added function which returns image bitmap from given url

--- src/main/kotlin/movieApiExample/network/QueryUtils.kt
+++ src/main/kotlin/movieApiExample/network/QueryUtils.kt
@@ -1,15 +1,9 @@
 package movieApiExample.network
 
 import org.jetbrains.skija.impl.Log
-import java.net.HttpURLConnection
 import java.net.MalformedURLException
 import java.net.URL
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.asImageBitmap
-import org.jetbrains.skija.Image
-import java.io.*
 import java.util.*
-import javax.imageio.ImageIO
 import javax.net.ssl.HttpsURLConnection
 
 
@@ -43,19 +37,4 @@ fun createUrl(stringUrl: String): String {
         Log.error("Problem building the URL - $e")
     }
     return getResponseFromHttpUrl(url)
-}
-
-fun loadNetworkImage(link: String?): ImageBitmap {
-    val url = URL(link)
-    val connection = url.openConnection() as HttpURLConnection
-    connection.connect()
-
-    val inputStream = connection.inputStream
-    val bufferedImage = ImageIO.read(inputStream)
-
-    val stream = ByteArrayOutputStream()
-    ImageIO.write(bufferedImage, "png", stream)
-    val byteArray = stream.toByteArray()
-
-    return Image.makeFromEncoded(byteArray).asImageBitmap()
 }
